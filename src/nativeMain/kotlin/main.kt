fbd79356e119b9d55624166f03e7f0dd3358faf7
Kirill Sadovnikov <d4rkm3z@gmail.com>
2020-10-10 20:53:43 +0300

    feat: implement user choice

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,2 +1,42 @@
+import kotlin.random.Random
+
 fun main() {
+    val options = arrayOf("Rock", "Paper", "Scissors")
+    val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
+}
+
+fun getGameChoice(optionsParam: Array<String>) =
+        optionsParam[(Random.nextInt(0, optionsParam.size))]
+
+fun getUserChoice(optionsParam: Array<String>): String {
+    printAllOptions(optionsParam)
+
+    val userChoice: Int = scanUserChoice(optionsParam)
+    return optionsParam[userChoice]
+}
+
+fun scanUserChoice(optionsParam: Array<String>): Int {
+    var userChoice: Int
+
+    do {
+        print("Enter your choice number: ")
+        userChoice = readLine()!!.toInt()
+
+        if (!validate(userChoice, optionsParam.size)) {
+            println("Incorrect choice, please try else...")
+        }
+    } while (!validate(userChoice, optionsParam.size))
+
+    println("\nYou selected: ${optionsParam[userChoice]}")
+    return userChoice
+}
+
+fun validate(userChoice: Int, size: Int): Boolean = userChoice < size
+
+fun printAllOptions (optionsParam: Array<String>) {
+    println("Select one of the following options:")
+    for (i in optionsParam.indices) {
+        println("$i: ${optionsParam[i]}")
+    }
 }
