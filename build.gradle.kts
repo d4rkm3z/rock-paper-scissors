a25888069a2efa1efc0f66855aafa03668b092fd
Kirill Sadovnikov <d4rkm3z@gmail.com>
2020-10-11 16:23:36 +0300

    feat: implement program logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ kotlin {
         binaries {
             executable {
                 entryPoint = "main"
+                runTask!!.standardInput = System.`in`
             }
         }
     }
--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,42 +1,63 @@
 import kotlin.random.Random
 
-fun main() {
-    val options = arrayOf("Rock", "Paper", "Scissors")
-    val gameChoice = getGameChoice(options)
-    val userChoice = getUserChoice(options)
+enum class Options {
+    ROCK, PAPER, SCISSORS
 }
 
-fun getGameChoice(optionsParam: Array<String>) =
-        optionsParam[(Random.nextInt(0, optionsParam.size))]
+fun main() {
+    val options = Options.values()
+    val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
 
-fun getUserChoice(optionsParam: Array<String>): String {
+    printResult(userChoice, gameChoice)
+}
+
+fun getGameChoice(optionsParam: Array<Options>) =
+    optionsParam[(Random.nextInt(0, optionsParam.size))]
+
+fun getUserChoice(optionsParam: Array<Options>): Options {
     printAllOptions(optionsParam)
 
     val userChoice: Int = scanUserChoice(optionsParam)
     return optionsParam[userChoice]
 }
 
-fun scanUserChoice(optionsParam: Array<String>): Int {
-    var userChoice: Int
+fun scanUserChoice(optionsParam: Array<Options>): Int {
+    var userChoice: Int = -1
 
     do {
         print("Enter your choice number: ")
-        userChoice = readLine()!!.toInt()
+        try {
+            val userInput = readLine()
+            if (userInput != null) userChoice = userInput.toInt()
+            else continue
+        } catch (error: NumberFormatException) {
+            continue
+        }
 
         if (!validate(userChoice, optionsParam.size)) {
             println("Incorrect choice, please try else...")
         }
     } while (!validate(userChoice, optionsParam.size))
 
-    println("\nYou selected: ${optionsParam[userChoice]}")
     return userChoice
 }
 
-fun validate(userChoice: Int, size: Int): Boolean = userChoice < size
+fun validate(userChoice: Int, size: Int): Boolean = userChoice in 0 until size
 
-fun printAllOptions (optionsParam: Array<String>) {
+fun printAllOptions(optionsParam: Array<Options>) {
     println("Select one of the following options:")
     for (i in optionsParam.indices) {
         println("$i: ${optionsParam[i]}")
     }
+}
+
+fun printResult(userChoice: Options, gameChoice: Options) {
+    val result: String = if (userChoice == gameChoice) "Tie!"
+    else if ((userChoice == Options.ROCK && gameChoice == Options.SCISSORS) ||
+        (userChoice == Options.PAPER && gameChoice == Options.ROCK) ||
+        (userChoice == Options.SCISSORS && gameChoice == Options.PAPER)) "You win!"
+    else "You lose!"
+
+    println("you chose $userChoice. I chose $gameChoice. $result")
 }
